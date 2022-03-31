5a53ff9f09968dfa37b2bc0d390af90c0e73b631
Anton21002 <97591679+Anton21002@users.noreply.github.com>
2022-03-31 10:16:24 +0300

    [WIP]Site feedback and connection update
    Added feedback to tell if the operation was sucessful and fixed the parameters of the methods ,also added mysql connection

--- src/main/java/siteEditor/RemovingData.java
+++ src/main/java/siteEditor/RemovingData.java
@@ -6,37 +6,59 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RemovingData {
 	
  private Connection conn;
+ 
 
 
-public int RemoveCandidate (Candidates candidate) {
+public void RemoveCandidate (HttpServletRequest request, HttpServletResponse response,Ehdokkaat ehdokas) 
+	      throws IOException, ServletException {
+	conn=java.sql.DriverManager.getConnection("jdbc:mysql://localhost:6033/vaalikone", "root", "password");
 int countRemcand = 0;
 String sql="DELETE FROM ehdokkaat WHERE EHDOKAS_ID=?";
 try {
-	PreparedStatement stmt = conn.prepareStatement(sql);
-	stmt.setInt(1,candidate.getEhdokasid);
 	
-} catch (SQLException e) {
+	PreparedStatement stmt = conn.prepareStatement(sql);
+	stmt.setInt(1,ehdokas.getEHDOKAS_ID);
+	
+	countRemcand=stmt.executeUpdate();
+} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
-
-
-return countRemcand;
+if(countRemcand==1)
+{
+System.out.println("Candidate Successfully Removed");	
 }
-public int RemoveQuestion (Questions question) {
-int countRemques = 0;
+
+
+conn.close();
+}
+public void RemoveQuestion (HttpServletRequest request, HttpServletResponse response,Questions question) 
+	      throws IOException, ServletException {
+	conn=java.sql.DriverManager.getConnection("jdbc:mysql://localhost:6033/vaalikone", "root", "password");
+int Remquesexec = 0;
 String sql="DELETE FROM kysymykset WHERE KYSYMYS_ID=?";
 try {
 	PreparedStatement stmt = conn.prepareStatement(sql);
 	stmt.setInt(1,question.getKYSYMYSID);
-	
-} catch (SQLException e) {
+	Remquesexec=stmt.executeUpdate() ;
+} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
+if(Remquesexec==1) {
+System.out.println("Question Successfully Removed");	
+}
+conn.close();
 }
 }
