819fff70f3de9c19a7934d610124718cad7438a4
Anton21002 <97591679+Anton21002@users.noreply.github.com>
2022-04-05 10:05:59 +0300

    [WIP] Added dependencies such as Dao and Kysymus
    Added it from kasmir branch so that i could test if it works

--- src/main/java/siteEditor/RemovingData.java
+++ src/main/java/siteEditor/RemovingData.java
@@ -16,8 +16,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-import app.dao.Dao;
-import app.model.Kysymys;
+import dao.Dao;
+import model.Kysymys;
 
 
 public class RemovingData {
@@ -67,4 +67,66 @@ System.out.println("Question Successfully Removed");
 }
 conn.close();
 }
+}
+public class RemoveKysymys extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException, ServletException {
+		
+		
+		// if sessions does not exist, create new one
+		HttpSession session = request.getSession();
+		
+		String idValue = request.getParameter("id");
+		
+		if ( idValue != null ) {
+			try {
+				int id = Integer.parseInt(idValue);
+				
+				Dao dao = new Dao();
+				Kysymys kysymys = dao.getKysymysInfo(id);
+				
+				session.setAttribute("kysymys", kysymys);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("jsp/removekysymys.jsp");
+				rd.forward(request, response);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// Back to list
+			response.sendRedirect("/showkysymys");
+			
+		}
+	
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException, ServletException {
+	
+		
+		// Create connection
+		Dao dao=new Dao();
+		Kysymys kysymys = readKysymys(request);
+		
+		dao.removeKysymys(kysymys);
+		
+		dao.close();
+		
+		// Back to list after actions
+		//RequestDispatcher rd = request.getRequestDispatcher("/showdata");
+		//rd.forward(request, response);
+		response.sendRedirect("/showkysymys");
+	}
+	
+	private Kysymys readKysymys(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		Kysymys kysymys=new Kysymys();
+		kysymys.setKYSYMYS(request.getParameter("breed"));
+		kysymys.setId(Integer.parseInt(request.getParameter("KYSYMYS_ID")));
+		return kysymys;
+	}
 }
